37283c3f543b1c47993f18cb03978b67f880df1c
CruzanCaramele <letters2hamza@gmail.com>
2019-02-09 00:25:01 +0800

    add installation requirements

--- weather/setup.py
+++ weather/setup.py
@@ -5,12 +5,12 @@ with open('README.md', encoding='UTF8') as f:
 
 setup (
 	name='weather',
-	version='0.1.0',
+	version='0.1.0',	
 	description='Command line weather tool',
 	long_description=readme,
 	author='Hamza',
 	author_email='letters2hamza@gmail.com',
-	install_requires=[],
+	install_requires=['requests>=2.0.0'],
 	packages=find_packages('src'),
 	package_dir={'','src'},
 	entry_points={
