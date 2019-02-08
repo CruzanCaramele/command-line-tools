76c91c550322d1443a6b7abf055d6657e850bce2
CruzanCaramele <letters2hamza@gmail.com>
2019-02-08 23:37:22 +0800

    re-align spaces

--- db_backup/setup.py
+++ db_backup/setup.py
@@ -4,9 +4,9 @@ with open('README.md', encoding='UTF-8') as f:
         readme = f.read()
 
 setup(
-        name ='pgbackup',
-        version ='0.1.0',
-        description ='Database backup to local folder or S3 bucket.',
+        name='pgbackup',
+        version='0.1.0',
+        description='Database backup to local folder or S3 bucket.',
         long_description=readme,
         author='Hamza',
         author_email='letters2hamza@gmail.com',
