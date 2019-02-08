dcabd8409e4eb8c550801be6ba3167d89944f3dd
CruzanCaramele <letters2hamza@gmail.com>
2019-02-09 00:37:53 +0800

    make api url and request for weather

--- weather/src/weather/cli.py
+++ weather/src/weather/cli.py
@@ -6,12 +6,12 @@ from argparse import ArgumentParser
 
 def make_api_url(args):
 	api_key = os.getenv('OWM_API_KEY')
-
 	if not api_key:
 		print('Error: OWM API KEY NOT FOUND\
 		      EXPORT API KEY AS ENV VARIABLE')
 		sys.exit(1)
-		url = 'https://openweathermap.org/api'
+		api_url = f"https://api.openweathermap.org/data/2.5/weather?zip={args.city},{args.country}&appid={api_key}"
+		return api_url
 
 def create_parser():
 	parser = ArgumentParser(description='Get the current weather information for your city')
@@ -22,4 +22,12 @@ def create_parser():
 
 def main():
 	args = create_parser().parse_args()
+	url = make_api_url(args)
+	res =  requests.get(url)
+	if res.status_code != 200:
+		print(f'Error reaching the weather provider: {res.status_code}')
+		sys.exit(1)
+	print(res.json())
+
+
 
