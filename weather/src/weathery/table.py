f5b9c4beae8dbdc5434042a793be260126508778
CruzanCaramele <letters2hamza@gmail.com>
2019-02-10 23:23:04 +0800

    convert temp from kelvin to celcius and farenhiet

--- weather/src/weathery/table.py
+++ weather/src/weathery/table.py
@@ -1,10 +1,24 @@
 from weathery import weather
 from veryprettytable import VeryPrettyTable
 
-HEADERS = ['City', 'Temperature', 'Forcast', 'Details']
+HEADERS = ['City', 'Temperature | Celcius', 'Temperature | Farenheit', 'Forecast', 'Details']
+
+def celcius(temp_kelvin):
+	int_temp = float(temp_kelvin)
+	to_celcius = int_temp - 273.15
+	return float("{0:.2f}".format(to_celcius))
+
+def farenheit(temp_kelvin):
+	int_temp = float(temp_kelvin)
+	to_farenheit = (int_temp - 273.15) * 9 / 5 + 32
+	return float("{0:.2f}".format(to_farenheit))
 
 def table(data):
 	x = VeryPrettyTable()
 	x.field_names = HEADERS
-	x.add_row([data["name"],data["main"]["temp"],data["weather"][0]["main"],data["weather"][0]["main"]])
+	temp_kelvin = data['main']['temp']
+	temp_celcius = celcius(temp_kelvin)
+	temp_farenheit = farenheit(temp_kelvin)
+
+	x.add_row([data['name'], temp_celcius , temp_farenheit, data['weather'][0]['main'],data['weather'][0]['description']])
 	return x
